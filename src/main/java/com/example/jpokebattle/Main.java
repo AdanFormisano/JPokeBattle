84b26f33a76fe6b6a2cf2124fd30b1838c698c37
AdanFormisano <formisano.1813917@studenti.uniroma1.it>
2025-01-30 18:39:29 +0100

    + Add MainUI class for game interface and update main entry point + Basic Main Menu UI assets

--- src/main/java/com/example/jpokebattle/Main.java
+++ src/main/java/com/example/jpokebattle/Main.java
@@ -1,17 +1,20 @@
 package com.example.jpokebattle;
 
+import com.example.jpokebattle.gui.MainUI;
 import com.example.jpokebattle.service.session.NoGuiSession;
 
 public class Main {
     public static void main(String[] args) {
 
-        // Create a new game session
-//        PokeSoloGameSession gameSession = new PokeSoloGameSession();
-        NoGuiSession gameSession = new NoGuiSession();
-
-        // Start the game
-        gameSession.startSession();
-        gameSession.playSession();
-        gameSession.endSession();
+        // Create a new nogui session
+        if (args.length > 0 && args[0].equals("nogui")) {
+            NoGuiSession gameSession = new NoGuiSession();
+            gameSession.startSession();
+            gameSession.playSession();
+            gameSession.endSession();
+            return;
+        } else {
+            MainUI.launch(MainUI.class, args);
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,9 @@ module com.example.jpokebattle {
     opens com.example.jpokebattle to javafx.fxml;
     exports com.example.jpokebattle;
 
+    opens com.example.jpokebattle.gui to javafx.fxml;
+    exports com.example.jpokebattle.gui;
+
     opens com.example.jpokebattle.service.data to com.fasterxml.jackson.databind;
     exports com.example.jpokebattle.service.data;
 
